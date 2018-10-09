d333b406cf067603f688350df48478c562f0cd7b
Derik Solis <derik98solis@gmail.com>
2018-10-09 14:39:13 -0400

    pushing updates

--- scripts.js
+++ scripts.js
@@ -37,51 +37,11 @@ function formValidator(e) {
         } 
     }
     
-};
+}; 
 
+// smooth slide 
 
-
-
-
-
-
-
-
-    // let fields = []
-    // fields[form["name"].name] = form["name"].value;
-    // fields[form["l-name"].name] = form["l-name"].value;
-    // fields[form["work"].name] = form["work"].value;
-    // console.log('FIRST WAY - ', fields);
-
-    // let formFields = {
-    //     name: form['name'].value,
-    //     lName: form['l-name'].value,
-    //     work: form['work'].value
-    // };
-    // console.log('SECOND WAY - ', formFields);
-
-    // let test = [];
-    // test.push({
-    //     name: form['name'].name,
-    //     val: form['name'].value
-    // });
-    // test.push({
-    //     name: form['l-name'].name,
-    //     val: form['l-name'].value
-    // });
-    // test.push({
-    //     name: form['work'].name,
-    //     val: form['work'].value
-    // });
-    // console.log('THIRD WAY - ', test);
-
-       
-    // for (let i = 0; i < Object.keys(fields).length ; i++) {
-    //     console.log('test');
-    //     console.log(fields[i]);
-    //     if (fields[i] === "") {
-    //         alert(`${fields[i]} must be filled out`);
-    //         console.log(`${fields[i]} must be filled out`);
-    //         return false;
-    //     }
-    // }
+function smoothSlide() {
+    let element = document.getElementById('arrow-down');
+    element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
